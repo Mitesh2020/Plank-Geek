7568324a010bcccddc526f1930d07f2dfb1e4f31
Mitesh2020 <miteshofficial007@gmail.com>
2023-04-23 11:04:50 +0530

    Minor changes in Footer

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
-<?php include 'googleTranslate.php'; ?>
+
 <html>
 <body>
-<?php generateGoogleTranslate(); ?>
 <footer class="bg-success" style="color:white;"><center>© 2023 Plant Geek</center></footer>
 </body>
 </html>
